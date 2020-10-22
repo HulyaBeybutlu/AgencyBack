71bbf188916edff995a44110c5a52f1b35136b90
HulyaBeybutlu <beybutluhulya98@gmail.com>
2020-10-22 11:41:51 +0400

    Add project files.

---
+++ AgencyBack/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AgencyBack.Models;
+
+namespace AgencyBack.Controllers
+{
+    public class HomeController : Controller
+    {
+        AgencyEntities db = new AgencyEntities();
+        public ActionResult Index()
+        {
+            ViewBag.section1 = db.Section1.First();
+            ViewBag.service = db.SectionServices.ToList();
+            ViewBag.portfolio = db.SectionPortfolios.ToList();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ AgencyBack/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AgencyBack.MvcApplication" Language="C#" %>
